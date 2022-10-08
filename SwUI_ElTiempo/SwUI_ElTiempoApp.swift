fc2521e053cd4accc4cd716082e76088cbef3907
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2022-10-08 04:03:43 +0200

    Initial Commit

---
+++ SwUI_ElTiempo/SwUI_ElTiempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUI_ElTiempoApp.swift
+//  SwUI_ElTiempo
+//
+//  Created by Miguel on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUI_ElTiempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
